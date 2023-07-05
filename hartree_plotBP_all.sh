f78b2519539f317b0fb6a399aea5ac970905fff2
rachapp <45595244+rachapp@users.noreply.github.com>
2023-07-05 08:55:22 +0100

    Create hartree_plotBP_all.sh
    Hartree version

---
+++ hartree_plotBP_all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#BSUB -o stdout.plot_BP.txt
+#BSUB -e stderr.plot_BP.txt
+#BSUB -R "span[ptile=16]"
+#BSUB -q scafellpikeSKL
+#BSUB -n 32
+#BSUB -J plot_BeamProfile
+#BSUB -W 8:00
+
+export PUFFDIR=$HCBASE/puffin/bin
+export py_script_directory=$HCBASE/plot-script
+export MYHOME=`pwd`
+export OMP_NUM_THREADS=1
+
+# setup modules
+. /etc/profile.d/modules.sh
+module load intel_mpi > /dev/null 2>&1
+module load intel
+module load python3/anaconda
+
+stdout_file="stdout.output.txt"
+stderr_file="stderr.output.txt"
+
+# delete the output file if it already exists
+if [ -f $stdout_file ]; then
+    rm $stdout_file
+fi
+
+# delete the output file if it already exists
+if [ -f $stderr_file ]; then
+    rm $stderr_file
+fi
+
+# iterate over the range of numbers
+# create the filename based on the pattern
+for i in {0..100}
+do
+    python3 /mnt/d/My_python_script/Modified_script/PlotBeamProfile.py f6microns_aperp_${i}.h5 >> $stdout_file 2>> $stderr_file
+done
